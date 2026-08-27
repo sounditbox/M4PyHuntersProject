cf7955c8b95c6e309a966cad2b9ebe38b9722e12
Ruslan <sounditbox@gmail.com>
2026-08-27 21:56:18 +0400

    feat: added all reiquired models fix: .gitkeep to keep folders in repo fix: add pillow lib for imagefields

--- products/models.py
+++ products/models.py
@@ -1,3 +1,57 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/products/category/{self.slug}'
+
+    def get_products(self):
+        return self.products.all()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save()
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, db_index=True)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT,
+                                 related_name='products')
+    price = models.DecimalField(max_digits=10, decimal_places=2,
+                                validators=[MinValueValidator(0)])
+    image = models.ImageField(upload_to='products/')
+    is_active = models.BooleanField(default=True)
+    stock = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/products/{self.slug}'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save()
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        ordering = ['-created_at']
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    product = models.ForeignKey('products.Product',
+                                on_delete=models.CASCADE,
+                                related_name='reviews')
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField(max_length=1000, blank=True, null=True)
+    image = models.ImageField(upload_to='reviews/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.owner} - {self.product}: {self.rating}'
+
+    def get_absolute_url(self):
+        return f'/reviews/{self.id}'
