326caca2886dd70fc5273e9c4d8eb5899cc07805
Ruslan <sounditbox@gmail.com>
2026-09-21 19:14:33 +0400

    fix(products): preserve save arguments

--- products/models.py
+++ products/models.py
@@ -21,7 +21,7 @@ class Category(models.Model):
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super().save()
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Category'
@@ -52,7 +52,7 @@ class Product(models.Model):
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super().save()
+        super().save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Product'
